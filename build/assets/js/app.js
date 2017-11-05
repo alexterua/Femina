bd9fd255590d9572a6f40f0c1196f62c8159e412
alexterua <alexter.od.ua@gmail.com>
2017-11-05 19:16:22 +0200

    slider specialists was edit and slider sertificates was added

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,11 +1,20 @@
 $(function() {  
 
-  $('.slider').slick({  
+  $('.slider__specialists').slick({  
   infinite: true,  
   slidesToShow: 4,
   slidesToScroll: 1,
   variableWidth: true   
   });
 
+  $('.slider__sertificates').slick({  
+    infinite: true,  
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true   
+    });
+
+  
+
 });
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uKCkgeyAgXHJcblxyXG4gICQoJy5zbGlkZXInKS5zbGljayh7ICBcclxuICBpbmZpbml0ZTogdHJ1ZSwgIFxyXG4gIHNsaWRlc1RvU2hvdzogNCxcclxuICBzbGlkZXNUb1Njcm9sbDogMSxcclxuICB2YXJpYWJsZVdpZHRoOiB0cnVlICAgXHJcbiAgfSk7XHJcblxyXG59KTsiXX0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uKCkgeyAgXHJcblxyXG4gICQoJy5zbGlkZXJfX3NwZWNpYWxpc3RzJykuc2xpY2soeyAgXHJcbiAgaW5maW5pdGU6IHRydWUsICBcclxuICBzbGlkZXNUb1Nob3c6IDQsXHJcbiAgc2xpZGVzVG9TY3JvbGw6IDEsXHJcbiAgdmFyaWFibGVXaWR0aDogdHJ1ZSAgIFxyXG4gIH0pO1xyXG5cclxuICAkKCcuc2xpZGVyX19zZXJ0aWZpY2F0ZXMnKS5zbGljayh7ICBcclxuICAgIGluZmluaXRlOiB0cnVlLCAgXHJcbiAgICBzbGlkZXNUb1Nob3c6IDQsXHJcbiAgICBzbGlkZXNUb1Njcm9sbDogMSxcclxuICAgIHZhcmlhYmxlV2lkdGg6IHRydWUgICBcclxuICAgIH0pO1xyXG5cclxuICBcclxuXHJcbn0pOyJdfQ==
--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,19 @@
 $(function() {  
 
-  $('.slider').slick({  
+  $('.slider__specialists').slick({  
   infinite: true,  
   slidesToShow: 4,
   slidesToScroll: 1,
   variableWidth: true   
   });
+
+  $('.slider__sertificates').slick({  
+    infinite: true,  
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true   
+    });
+
+  
 
 });
